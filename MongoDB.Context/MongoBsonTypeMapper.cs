47e990e9a511d337f3d32e50c3a10089468eebc3
cadams93 <callumadams230@gmail.com>
2016-08-10 20:47:16 +1000

    Fixed: Use BsonSerialiser.Deserialise rather than manually rehydrating a .NET type Quality: Multiple consecutive array item removals now result in just one additional null item Pull

---
+++ MongoDB.Context/MongoBsonTypeMapper.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB.Context
+{
+	public static class MongoBsonTypeMapper<TDocument, TIdField>
+		where TDocument : AbstractMongoEntityWithId<TIdField>
+	{
+		public static object GetDotNetValue(object[] fieldPath, BsonValue value)
+		{
+			// Simple type
+			if (!value.IsBsonDocument && !value.IsBsonArray)
+				return BsonTypeMapper.MapToDotNetValue(value);
+
+			// Array (of either a simple type or document)
+			if (value.IsBsonArray)
+			{
+				return value.AsBsonArray
+					.Select((t, idx) => GetDotNetValue(fieldPath.Concat(new object[] { idx }).ToArray(), t))
+					.ToArray();
+			}
+
+			// Here, we are a BSON document (most likely a new .NET type)
+			Type type;
+			if (TryGetTypeOfField(fieldPath, out type)) 
+				return BsonSerializer.Deserialize(value.AsBsonDocument, type);
+			
+			// We failed to find the .NET type from the TDocument class
+			// ie. the BSON document now has something unmapped in the class (ExtraElements)
+			return BsonTypeMapper.MapToDotNetValue(value);
+		}
+
+		private static bool TryGetTypeOfField(object[] fieldPath, out Type type)
+		{
+			var queue = new Queue<object>(fieldPath);
+
+			type = null;
+			while (queue.Any())
+			{
+				var field = queue.Dequeue();
+
+				var arrayIndex = field as int?;
+
+				if (arrayIndex.HasValue)
+				{
+					if (type == null || type.GetInterfaces().All(z => !z.IsGenericType || z.GetGenericTypeDefinition() != typeof(IEnumerable<>)))
+					{
+						LogFailureIfDebug(fieldPath);
+						return false;
+					}
+
+					var enumerableInterface = type.GetInterfaces().Single(z => z.IsGenericType && z.GetGenericTypeDefinition() == typeof(IEnumerable<>));
+					type = enumerableInterface.GetGenericArguments()[0];
+					continue;
+				}
+
+				var fieldName = field as string;
+
+				var member = (type ?? typeof(TDocument))
+					.GetMembers()
+					.SingleOrDefault(z =>
+						z.CustomAttributes.All(a => a.AttributeType != typeof(BsonIgnoreAttribute))
+						&& (z.Name == fieldName || (
+							z.GetCustomAttributes(typeof(BsonElementAttribute)).Any()
+							&& ((BsonElementAttribute)z.GetCustomAttributes(typeof(BsonElementAttribute)).Single()).ElementName == fieldName)
+						)
+					);
+
+				if (member == null)
+				{
+					LogFailureIfDebug(fieldPath);
+					return false;
+				}
+
+				var propInfo = member as PropertyInfo;
+				if (propInfo != null)
+				{
+					type = propInfo.PropertyType;
+					continue;
+				}
+
+				var fieldInfo = member as FieldInfo;
+				if (fieldInfo != null)
+				{
+					type = fieldInfo.FieldType;
+					continue;
+				}
+
+				LogFailureIfDebug(fieldPath);
+				return false;
+			}
+
+			return true;
+		}
+
+		private static void LogFailureIfDebug(object[] fieldPath)
+		{
+#if DEBUG
+			System.Diagnostics.Debug.WriteLine(string.Format("MongoBsonTypeMapper: Failed to get .NET type for {0}", string.Join(".", fieldPath)));
+#endif
+		}
+	}
+}
--- MongoDB.Context/MongoChangeFactory.cs
+++ MongoDB.Context/MongoChangeFactory.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Reflection;
-using System.Xml.Schema;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Context.Bson.Differences;
 using MongoDB.Context.Locking;
 using MongoDB.Driver;
@@ -15,7 +10,9 @@ namespace MongoDB.Context
 	public class MongoChangeFactory<TDocument, TIdField>
 		where TDocument : AbstractMongoEntityWithId<TIdField>
 	{
-		public MongoChange<TDocument, TIdField>[] GetMongoChangesFromChangeSet(MongoCollectionChangeSet<TDocument, TIdField> collectionChangeSet, out List<MongoLockRequest<TIdField>> locksRequired)
+		public MongoChange<TDocument, TIdField>[] GetMongoChangesFromChangeSet(
+			MongoCollectionChangeSet<TDocument, TIdField> collectionChangeSet, 
+			out List<MongoLockRequest<TIdField>> locksRequired)
 		{
 			locksRequired = new List<MongoLockRequest<TIdField>>();
 
@@ -67,8 +64,63 @@ namespace MongoDB.Context
 						// We have to be mindful of the order of operations to ensure index references are correct
 						// Iterate over the changes in order in which they were added to the collection
 						var order = 1;
+						var nullPullRequired = false;
 						foreach (var change in differenceByField.Value)
 						{
+							var arrayChange = change as BsonArrayItemDifference<TDocument, TIdField>;
+						
+							// If this change is NOT another array item removal, we need to pull all nulls from the array
+							if (nullPullRequired && (arrayChange == null || arrayChange.Type != BsonArrayItemDifferenceType.Remove))
+							{
+								mongoChanges.Add(new MongoChange<TDocument, TIdField>
+								{
+									Change = new UpdateOneModel<TDocument>(
+										Builders<TDocument>.Filter.Eq(z => z._Id, document._Id),
+										Builders<TDocument>.Update.Pull(differenceByField.Key, (object)null)
+									),
+									ExecutionOrder = ++order
+								});
+
+								nullPullRequired = false;
+							}
+
+							if (arrayChange != null)
+							{
+								switch (arrayChange.Type)
+								{
+									case BsonArrayItemDifferenceType.Add:
+										var newValue = MongoBsonTypeMapper<TDocument, TIdField>
+											.GetDotNetValue(arrayChange.FieldPath, arrayChange.ArrayItem);
+
+										mongoChanges.Add(new MongoChange<TDocument, TIdField>
+										{
+											Change = new UpdateOneModel<TDocument>(
+												Builders<TDocument>.Filter.Eq(z => z._Id, document._Id),
+												Builders<TDocument>.Update.PushEach(differenceByField.Key, new[] { newValue },
+													position: (int)arrayChange.FieldPath.Last())
+											),
+											ExecutionOrder = ++order
+										});
+										break;
+									case BsonArrayItemDifferenceType.Remove:
+										var elementPath = string.Join(".", arrayChange.FieldPath);
+										mongoChanges.Add(new MongoChange<TDocument, TIdField>
+										{
+											Change = new UpdateOneModel<TDocument>(
+												Builders<TDocument>.Filter.Eq(z => z._Id, document._Id),
+												Builders<TDocument>.Update.Unset(elementPath)
+											),
+											ExecutionOrder = ++order
+										});
+										nullPullRequired = true;
+										break;
+									default:
+										throw new ArgumentOutOfRangeException();
+								}
+
+								continue;
+							}
+
 							var fieldChange = change as BsonFieldDifference<TDocument, TIdField>;
 							if (fieldChange != null)
 							{
@@ -98,7 +150,9 @@ namespace MongoDB.Context
 									continue;
 								}
 
-								var newValue = GetDotNetValue(fieldChange.FieldPath, fieldChange.NewValue);
+								var newValue = MongoBsonTypeMapper<TDocument, TIdField>
+									.GetDotNetValue(fieldChange.FieldPath, fieldChange.NewValue);
+
 								if (isRootFieldChange)
 								{
 									fieldUpdates = fieldUpdates == null
@@ -112,51 +166,9 @@ namespace MongoDB.Context
 										Change = new UpdateOneModel<TDocument>(
 											Builders<TDocument>.Filter.Eq(z => z._Id, document._Id),
 											Builders<TDocument>.Update.Set(elementPath, newValue)
-											),
+										),
 										ExecutionOrder = ++order
 									});
-								}
-
-								continue;
-							}
-
-							var arrayChange = change as BsonArrayItemDifference<TDocument, TIdField>;
-							if (arrayChange != null)
-							{
-								switch (arrayChange.Type)
-								{
-									case BsonArrayItemDifferenceType.Add:
-										var newValue = GetDotNetValue(arrayChange.FieldPath, arrayChange.ArrayItem);
-										mongoChanges.Add(new MongoChange<TDocument, TIdField>
-										{
-											Change = new UpdateOneModel<TDocument>(
-												Builders<TDocument>.Filter.Eq(z => z._Id, document._Id),
-												Builders<TDocument>.Update.PushEach(differenceByField.Key, new[] { newValue }, position: (int)arrayChange.FieldPath.Last())
-											),
-											ExecutionOrder = ++order
-										});
-										break;
-									case BsonArrayItemDifferenceType.Remove:
-										var elementPath = string.Join(".", arrayChange.FieldPath);
-										mongoChanges.Add(new MongoChange<TDocument, TIdField>
-										{
-											Change = new UpdateOneModel<TDocument>(
-												Builders<TDocument>.Filter.Eq(z => z._Id, document._Id),
-												Builders<TDocument>.Update.Unset(elementPath)
-											),
-											ExecutionOrder = ++order
-										});
-										mongoChanges.Add(new MongoChange<TDocument, TIdField>
-										{
-											Change = new UpdateOneModel<TDocument>(
-												Builders<TDocument>.Filter.Eq(z => z._Id, document._Id),
-												Builders<TDocument>.Update.Pull(differenceByField.Key, (object)null)
-											),
-											ExecutionOrder = ++order
-										});
-										break;
-									default:
-										throw new ArgumentOutOfRangeException();
 								}
 
 								continue;
@@ -180,119 +192,6 @@ namespace MongoDB.Context
 			}
 
 			return mongoChanges.ToArray();
-		}
-
-		private static object GetDotNetValue(object[] fieldPath, BsonValue value)
-		{
-			// Simple type
-			if (!value.IsBsonDocument && !value.IsBsonArray)
-			{
-				return BsonTypeMapper.MapToDotNetValue(value);
-			}
-
-			// Array (of either a simple type or document)
-			if (value.IsBsonArray)
-			{
-				var bsonArray = value.AsBsonArray;
-				return bsonArray.Select((t, idx) => GetDotNetValue(fieldPath.Concat(new object[] {idx}).ToArray(), t)).ToArray();
-			}
-
-			// Here, we are a BSON document (most likely a new .NET type)
-			var type = GetTypeOfField(fieldPath);
-
-			var requiredMembers = value.AsBsonDocument.Names.ToArray();
-			var memberDict = type.GetMembers()
-				.Where(z =>
-					z.CustomAttributes.All(a => a.AttributeType != typeof(BsonIgnoreAttribute))
-					&& (requiredMembers.Contains(z.Name) || (
-						z.GetCustomAttributes(typeof(BsonElementAttribute)).Any()
-						&& requiredMembers.Contains(((BsonElementAttribute)z.GetCustomAttributes(typeof(BsonElementAttribute)).Single()).ElementName))
-						)
-				)
-				.ToDictionary(z =>
-				{
-					if (requiredMembers.Contains(z.Name)) return z.Name;
-					var bsonElementAttribute = (BsonElementAttribute)z.GetCustomAttributes(typeof(BsonElementAttribute)).Single();
-					return bsonElementAttribute.ElementName;
-				});
-
-			//Rehydrate an instance of the given type
-			var instance = Activator.CreateInstance(type);
-			foreach (var element in value.AsBsonDocument.Elements)
-			{
-				var member = memberDict[element.Name];
-
-				var propInfo = member as PropertyInfo;
-				if (propInfo != null)
-				{
-					propInfo.SetValue(instance, BsonTypeMapper.MapToDotNetValue(element.Value));
-					continue;
-				}
-
-				var fieldInfo = member as FieldInfo;
-				if (fieldInfo != null)
-				{
-					fieldInfo.SetValue(instance, BsonTypeMapper.MapToDotNetValue(element.Value));
-					continue;
-				}
-			}
-
-			return instance;
-		}
-
-		private static Type GetTypeOfField(object[] fieldPath)
-		{
-			var queue = new Queue<object>(fieldPath);
-
-			Type tempType = null;
-			while (queue.Any())
-			{
-				var field = queue.Dequeue();
-
-				var fieldName = field as string;
-				var arrayIndex = field as int?;
-
-				if (fieldName == null && arrayIndex.HasValue)
-				{
-					if (tempType == null || tempType.GetInterfaces().All(z => !z.IsGenericType || z.GetGenericTypeDefinition() != typeof(IEnumerable<>)))
-						throw new Exception(string.Format("Cannot find C# type for {0}", string.Join(".", fieldPath)));
-
-					var enumerableInterface = tempType.GetInterfaces().Single(z => z.IsGenericType && z.GetGenericTypeDefinition() == typeof(IEnumerable<>));
-					tempType = enumerableInterface.GetGenericArguments()[0];
-					continue;
-				}
-
-				var member = (tempType ?? typeof(TDocument))
-					.GetMembers()
-					.SingleOrDefault(z =>
-						z.CustomAttributes.All(a => a.AttributeType != typeof(BsonIgnoreAttribute))
-						&& (z.Name == fieldName || (
-							z.GetCustomAttributes(typeof(BsonElementAttribute)).Any()
-							&& ((BsonElementAttribute)z.GetCustomAttributes(typeof(BsonElementAttribute)).Single()).ElementName == fieldName)
-						)
-					);
-
-				if (member == null)
-					throw new Exception(string.Format("Cannot find C# type for {0}", string.Join(".", fieldPath)));
-
-				var propInfo  = member as PropertyInfo;
-				if (propInfo != null)
-				{
-					tempType = propInfo.PropertyType;
-					continue;
-				}
-
-				var fieldInfo = member as FieldInfo;
-				if (fieldInfo != null)
-				{
-					tempType = fieldInfo.FieldType;
-					continue;
-				}
-
-				throw new Exception(string.Format("Cannot find C# type for {0}", string.Join(".", fieldPath)));
-			}
-
-			return tempType;
 		}
 	}
 }
